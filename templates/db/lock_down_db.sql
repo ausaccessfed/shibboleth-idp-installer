0dd3350d6bd988551af59681d3f8e8495e23bcc6
Russell Ianniello <russell.ianniello@aaf.edu.au>
2015-07-16 08:31:50 +1000

    Restructured mariadb/db property

--- templates/db/lock_down_db.sql
+++ templates/db/lock_down_db.sql
@@ -1,6 +1,6 @@
-GRANT SUPER ON *.* TO 'root'@'localhost' IDENTIFIED BY '{{ mariadb.password }}';
-GRANT SUPER ON *.* TO 'root'@'127.0.0.1' IDENTIFIED BY '{{ mariadb.password }}';
-GRANT SUPER ON *.* TO 'root'@'::1' IDENTIFIED BY '{{ mariadb.password }}';
+GRANT SUPER ON *.* TO 'root'@'localhost' IDENTIFIED BY '{{ db.password }}';
+GRANT SUPER ON *.* TO 'root'@'127.0.0.1' IDENTIFIED BY '{{ db.password }}';
+GRANT SUPER ON *.* TO 'root'@'::1' IDENTIFIED BY '{{ db.password }}';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE test;
