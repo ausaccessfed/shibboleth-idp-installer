4456f3348a252c2f835dac721ee05d4bb2e884be
Bradley Beddoes <bradleybeddoes@gmail.com>
2015-09-30 09:59:21 +1000

    Use ANT_OPTS to call Shibboleth install script

--- templates/idp/install.sh
+++ templates/idp/install.sh
@@ -2,6 +2,10 @@
 set -e
 export ANT_OPTS=" \
       -Didp.src.dir={{ shib_idp.src }} \
-      -Didp.target.dir={{ shib_idp.home }}"
+      -Didp.target.dir={{ shib_idp.home }} \
+      -Didp.host.name={{ idp_host_name }} \
+      -Didp.scope={{ idp_attribute_scope }} \
+      -Didp.keystore.password={{ shib_idp.keystore_password }} \
+      -Didp.sealer.password={{ shib_idp.cookie_enc_key_password }} "
 export JAVA_HOME="/usr/lib/jvm/java"
 sh {{ shib_idp.src }}/bin/install.sh
